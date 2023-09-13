48cf9533bf7058eb8cf3c9d47d882639553f28ac
mr-mensah <mr-mensah@gmail.com>
2023-09-13 20:23:24 +0000

    delete

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,12 @@
-#!/usr/bin/python3
-def to_subtract(list_num):
+ubtract(list_num):
     to_sub = 0
     max_list = max(list_num)
     for n in list_num:
         if max_list > n:
             to_sub += n
     return (max_list - to_sub)
+
+
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
@@ -27,3 +28,4 @@ def roman_to_int(roman_string):
                 last_rom = rom_n.get(ch)
     num += to_subtract(list_num)
     return (num)
+
