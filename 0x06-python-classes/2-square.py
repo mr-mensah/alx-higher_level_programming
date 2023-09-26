37bc40c0f5e1df4abd7f6f15e3f420f6704af890
mr-mensah <mr-mensah@gmail.com>
2023-09-26 21:34:33 +0000

    errors

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-
+"""2. Size validation """
 
 
 class Square:
+    """denotes a square. """
 
     def __init__(self, size=0):
-    
+        """Using the __init__ method.
+
+        Args:
+            size: private instance attribute for my_square
+        """
         if isinstance(size, int) is False:
             raise TypeError("size must be an integer")
         if size < 0:
