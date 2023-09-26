d397ccaff7cbc30518b76757be73c9813cede9c7
mr-mensah <mr-mensah@gmail.com>
2023-09-26 21:16:57 +0000

    more squares

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""1. Square with size """
+
+class Square:
+    """Define square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
