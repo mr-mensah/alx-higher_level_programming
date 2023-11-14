9d55822e7be1887be55d6037dd849005c96e4728
mr-mensah <mr-mensah@gmail.com>
2023-11-14 12:30:04 -0800

    mandatory

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- script that creates a table
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+
+-- script that adds multiple rows
+INSERT INTO second_table VALUES
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
