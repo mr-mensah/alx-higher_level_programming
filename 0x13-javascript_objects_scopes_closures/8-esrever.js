129a31ce62b9e0b8f9568a7949ed55e988e05e68
mr-mensah <mr-mensah@gmail.com>
2023-12-12 13:40:08 -0800

    mandatory

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  let start = 0;
+  let end = list.length - 1;
+
+  while (start < end) {
+    const temp = list[start];
+    list[start] = list[end];
+    list[end] = temp;
+
+    start += 1;
+    end -= 1;
+  }
+  return list;
+};
